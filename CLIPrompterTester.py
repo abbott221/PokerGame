86b7e8677f80971b6e853f9dfe15038c8243c887
trb139 <abderrahman.said-alaoui@capitalone.com>
2017-07-20 16:29:18 -0400

    prompt user for number of players

--- CLIPrompterTester.py
+++ CLIPrompterTester.py
@@ -13,19 +13,8 @@ class CLIPrompterTester(unittest.TestCase):
     def test_printHand_PrintHandContent(self):
         self.assertEqual(self.cliPrompt.printHand(1), '2-H, 7-C, K-C, 5-D, J-S') 
 
-    # def test_upper(self):
-    #     self.assertEqual('foo'.upper(), 'FOO')
-
-    # def test_isupper(self):
-    #     self.assertTrue('FOO'.isupper())
-    #     self.assertFalse('Foo'.isupper())
-
-    # def test_split(self):
-    #     s = 'hello world'
-    #     self.assertEqual(s.split(), ['hello', 'world'])
-    #     # check that s.split fails when the separator is not a string
-    #     with self.assertRaises(TypeError):
-    #         s.split(2)
+    def test_promptNumUsers_Return5(self):
+        self.assertEqual(self.cliPrompt.promptNumberPlayers(), 2)
 
 if __name__ == '__main__':
     unittest.main()
