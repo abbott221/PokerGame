d7d5739db029f02e01f39520a0e004d837dc7085
Mehr <orb827@784f43a2f9ee.cof.ds.capitalone.com>
2017-07-21 08:58:25 -0400

    Added comparator for hands and began labelling hand values

--- CardTester.py
+++ CardTester.py
@@ -5,7 +5,7 @@ import Card
 # Example class for setting up unit testing in python
 class CardTester(unittest.TestCase):
 
-    def testAceLessThanKing(self):
+    def aceLessThanKing(self):
         card1 = Card.Card(Card.Rank.ACE, Card.Suit.SPADES)
         card2 = Card.Card(Card.Rank.KING, Card.Suit.DIAMONDS)
         self.assertTrue(card1.rank > card2.rank)
--- Hand.py
+++ Hand.py
@@ -1,4 +1,88 @@
 import Card
+from enum import IntEnum
+
+class Value(IntEnum):
+	HIGH_CARD = 1
+	PAIR = 2
+	TWO_PAIR = 3
+	THREE_OF_A_KIND = 4
+	FULL_HOUSE = 5
+	STRAIGHT = 6
+	FLUSH = 7
+	FOUR_OF_A_KIND = 8
+	STRAIGHT_FLUSH = 9
+
 class Hand:
 	def __init__(self, cards):
 		self.cards = sorted(cards, key=lambda x: x.rank, reverse=True)
+		self.value = 0;
+		self.tiebreaker = [0] * 5
+		if self.isStraightFlush():
+			self.labelStraightFlush()
+		elif self.isFourOfAKind():
+			self.labelFourOfAKind()
+		elif self.isFlush():
+			self.labelFlush()
+		elif self.isStraight():
+			self.labelStraight()
+		elif self.isFullHouse():
+			self.labelFullHouse()
+		elif self.isThreeOfAKind():
+			self.labelThreeOfAKind() 
+		elif self.isTwoPair():
+			self.labelTwoPair() 
+		elif self.isPair():
+			self.labelPair() 
+		else:
+			self.labelHighCard()
+
+	def compareTo(self, hand2):
+		if self.value > hand2.value:
+			return 1
+		elif self.value < hand2.value:
+			return -1
+		else:
+			for i in range(0, 4):
+				if self.tiebreaker[i] > hand2.tiebreaker[i]:
+					return 1
+				elif self.tiebreaker[i] < hand2.tiebreaker[i]:
+					return -1
+			return 0
+
+
+	def isStraightFlush(self):
+		return
+	def labelStraightFlush(self):
+		return
+	def isFourOfAKind(self):
+		return
+	def labelFourOfAKind(self):
+		return
+	def isFlush(self):
+		return
+	def labelFlush(self):
+		return
+	def isStraight(self):
+		return
+	def labelStraight(self):
+		return
+	def isFullHouse(self):
+		return
+	def labelFullHouse(self):
+		return
+	def isThreeOfAKind(self):
+		return
+	def labelThreeOfAKind(self):
+		return 
+	def isTwoPair(self):
+		return
+	def labelTwoPair(self):
+		return 
+	def isPair(self):
+		return
+	def labelPair(self):
+		return 
+	def labelHighCard(self):
+		self.value = Value.HIGH_CARD
+		for i in range(0, 4):
+			self.tiebreaker[i] = self.cards[i].rank
