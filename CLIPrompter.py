f99013ff7d51bf38502a46a7ff2b05749d53d34d
trb139 <abderrahman.said-alaoui@capitalone.com>
2017-07-21 08:35:48 -0400

    just realized been dev'ng in master, moving to dev branch

--- CLIPrompter.py
+++ CLIPrompter.py
@@ -1,4 +1,5 @@
-
+from Card import Rank, Suit
+from Hand import Hand
 
 class CLIPrompter():
 
@@ -7,6 +8,12 @@ class CLIPrompter():
 		num = int(input('How many humans are playing today? \nValid values are 2, 3, or 4.\n'))
 		return num if ( 1 < num < 5) else -1
 
+	def printHand(self, hand):
+		for card in hand.cards[:-1]:
+			print(str(card.rank)+'-'+str(card.suit[0])+', '),
+		print(str(card.rank)+'-'+str(card.suit[0]))
+
 if __name__ == '__main__':
 	prompter = CLIPrompter()
-	prompter.promptNumberPlayers()
+	val = prompter.promptNumberPlayers()
+	print(val)
--- CLIPrompterTester.py
+++ CLIPrompterTester.py
@@ -1,17 +1,28 @@
 import unittest
 import CLIPrompter
-# import Game
+from Card import Rank, Suit, Card
+from Hand import Hand
 
 class CLIPrompterTester(unittest.TestCase):
     def setUp(self):
         # self.game = Game()
         self.cliPrompt = CLIPrompter.CLIPrompter()
+        self.cards = [Card(Rank.TWO, Suit.HEARTS), Card(Rank.SEVEN, Suit.CLUBS), 
+        Card(Rank.KING, Suit.CLUBS), Card(Rank.FIVE, Suit.DIAMONDS), 
+        Card(Rank.JACK, Suit.SPADES)]
+        self.hand = Hand(self.cards)
 
     def test_printHand_PrintHandContent(self):
-        self.assertEqual(self.cliPrompt.printHand(1), '2-H, 7-C, K-C, 5-D, J-S') 
+        self.assertEqual(self.cliPrompt.printHand(self.hand.cards), '2-H, 7-C, K-C, 5-D, J-S') 
 
-    def test_promptNumUsers_Return5(self):
+    def test_promptNumUsers_Return2(self):
+        print('return 2 test')
         self.assertEqual(self.cliPrompt.promptNumberPlayers(), 2)
+
+    def test_promptNumUsers_ReturnNeg1(self):
+        print('return -1 test')
+        self.assertEqual(self.cliPrompt.promptNumberPlayers(), -1)
+
 
 if __name__ == '__main__':
     unittest.main()
